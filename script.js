a44cd48f6aba1975614a34acc847080a83c7c840
Preetham Praveen <preethampraveencode26@gmail.com>
2025-04-08 15:47:56 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const jwt = require('jsonwebtoken');
 const encrypt = (payload, secret) => {
   // your code here and return token
+  return jwt.sign(payload,secret, {expiresIn:'7d'});
 };
 
 module.exports = encrypt;
